90d612f4b82a991b8b108d6b05b44b2e4bef7159
robertobrigantino-blip <robertobrigantino@gmail.com>
2026-03-10 22:38:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -55,14 +55,14 @@ URL_COUNTER = "https://hits.sh/robertobrigantino-blip.github.io/todis-volley.svg
 
 # ================= CAMPIONATI (STAGIONE REGOLARE) =================
 CAMPIONATI_MASCHILI = {
-    "Serie D  Gir.C S.Maschile": "85622",
+    "Serie D Gir.C S.Maschile": "85622",
     "U19 Gir.A S.Maschile": "86865",
     "U17 Gir.B S.Maschile": "86864",
     "U15 Gir.B S.Maschile": "86848",
 }
 
 CAMPIONATI_FEMMINILI = {
-    "Serie C  Gir.A S.Femminile": "85471",
+    "Serie C Gir.A S.Femminile": "85471",
     "U18 Gir.B S.Femminile": "86850",
     "U16 Gir.A S.Femminile": "86853",
     "U14 Gir.C S.Femminile": "86860",
@@ -77,8 +77,8 @@ FASI_FINALI = {
 
 # ================= PLAY OUT SALVEZZA (MINI-GIRONI CON CLASSIFICA) =================
 PLAY_OUT = {
-    "Serie C  Gir.A S.Femminile": "88812",
-    "Serie D  Gir.C S.Maschile": "88826",
+    "Serie C Gir.A S.Femminile": "88812",
+    "Serie D Gir.C S.Maschile": "88826",
 }
 
 # ================= CAMPIONATI FINITI (STAGIONE CONCLUSA) =================
@@ -89,11 +89,11 @@ CAMPIONATI_FINITI =[
 
 # Mappa dei campionati che hanno una classifica generale avulsa
 CAMPIONATI_AVULSI = {
-    "Serie C  Gir.A S.Femminile": "85473",
+    "Serie C Gir.A S.Femminile": "85473",
     "U14 Gir.C S.Femminile": "86858",
     "U16 Gir.A S.Femminile": "86853",
     "U18 Gir.B S.Femminile": "86849",
-    "Serie D  Gir.C S.Maschile": "85620",
+    "Serie D Gir.C S.Maschile": "85620",
     "U19 Gir.A S.Maschile": "86865",
 }
 
@@ -921,3 +921,4 @@ if __name__ == "__main__":
     genera_pagina_generale(df_ris, df_class, FILE_GEN_FEMALE, CAMPIONATI_FEMMINILI, FILE_FEMALE)
     genera_segnapunti()
     print(f"✅ Generazione {APP_VERSION} completata!")
+
