8989075634220a342c785f51febeccf9c4c149e0
Roberto <robertobrigantino@gmail.com>
2026-09-16 18:04:07 +0200

    aggiunta foto

--- main.py
+++ main.py
@@ -91,6 +91,7 @@ GALLERIA_MEDIA = {
     ],
     "U12 S.Femminile": [
         {"type": "image", "url": "https://raw.githubusercontent.com/robertobrigantino-blip/todis-volley/main/Foto/IMG_1161.jpg", "caption": "Premiazione U12 2025"},
+        {"type": "image", "url": "https://raw.githubusercontent.com/robertobrigantino-blip/todis-volley/main/Foto/U12_S2026.png", "caption": "1° Classificate U12 2026"},
         {"type": "image", "url": "https://raw.githubusercontent.com/robertobrigantino-blip/todis-volley/main/Foto/IMG_3487.jpg", "caption": "Premiazione U12 2026"},
     ],
     "U13 S.Femminile": [
